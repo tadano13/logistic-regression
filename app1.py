82b4eb80a9c86bc696b5e8826348742651a8e8d0
tadano <nishantcoclover@gmail.com>
2024-07-11 20:23:49 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,40 +1,54 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import pickle
+import pandas as pd
+import joblib
 
-# Load the model
-clf = pickle.load(open("titanic_model.pkl","rb"))
+
+clf = joblib.load(open("titanic_model.pkl","rb"))
 
 def predict(data):
-    clf = pickle.load(open("titanic_model.pkl","rb"))
+    clf = joblib.load(open("titanic_model.pkls","rb"))
     return clf.predict(data)
 
+st.title("Titanic Passenger Survival Prediction")
+st.markdown("This model predicts whether a Titanic passenger survived or not based on input features.")
 
-st.title("Titanic Survival Prediction using Machine Learning")
-st.markdown("This Model Identify weather people survived or not")
 
-st.header("Passenger Details")
-col1,col2 = st.columns(2)
+st.header("Traveler Details")
+
+
+col1, col2 = st.columns(2)
+
 
 with col1:
-	st.text("Age")
-	age = st.slider("Age", 1.0, 100.0, 0.5)
-	st.text("fare")
-	fare = st.slider("Ticket Fare", 0.0, 10000.0, 0.5)
-	st.text("family Size")
-	fs = st.slider("Family Size", 1,10,2)
+    age = st.slider("Age", 1, 100, 25)
+    fare = st.slider("Ticket Fare", 0.0, 10000.0, 50.0)
+    fs = st.slider("Family Size", 1, 10, 2)
+
+
 with col2:
-	st.text("class 1: First, 2: 2nd,3:3rd")
-	pc=st.selectbox("Passenger Class:",[1,2,3])
-	st.text("Boarding From 1:Chernourg , 2:Queenstown ,3:Southhampton")
-	bc=st.selectbox("Boarding From:",[1,2,3])
+    pc = st.selectbox("Passenger Class", ["First Class", "Second Class", "Third Class"])
+    bc = st.selectbox("Boarding From", ["Cherbourg", "Queenstown", "Southampton"])
 
+# Convert passenger class and boarding location to numerical values
+# For passenger class
+if pc == "First Class":
+    pc = 1
+elif pc == "Second Class":
+    pc = 2
+else:
+    pc = 3
 
-st.text('')
-if st.button("Predict Survied Or Not "):
-    result = clf.predict(
-        np.array([[age,fare,fs,pc,bc,1,1,1]]))
+# For boarding location
+if bc == "Cherbourg":
+    boarding = 1
+elif bc == "Queenstown":
+    boarding = 2
+else:
+    boarding = 3
+
+# Prediction button
+if st.button("Predict Survival"):
+    result = clf.predict(np.array([[age,fare,fs,pc,boarding,1,1,1]]))
     st.text(result[0])
-
-st.markdown("Developed By Nishant Sawaimoon at NIELIT Daman")
+   
